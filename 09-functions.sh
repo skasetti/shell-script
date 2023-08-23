a147afdaaebc725ee49ada82bc4fc0a886b99528
srinivas <skasetti@gmail.com>
2023-08-23 19:11:39 -0400

    Adding functions

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,16 +6,20 @@ DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 # this function should validate the previous command and inform user it is success or failure
 
 VALIDATE(){
     #$1 --> it will receive the argument
     if [ $1 -ne 0 ]
 then
-    echo "$2 .... FAILURE"
+    echo -e "$2 .... $R FAILURE $N"
     exit 1
 else
-echo "$2 .... SUCCESS"
+echo -e "$2 .... $G SUCCESS $N"
 fi
 }
 
