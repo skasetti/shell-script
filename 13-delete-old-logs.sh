c11d7f43a90c5f7eca28444bb7e9cc44f4efc629
srinivas <skasetti@gmail.com>
2023-08-25 14:41:32 -0400

    Finctions testing

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -10,7 +10,7 @@ LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
-echo "script started executing at $DATE"
+echo "script started executing at $DATE" &>> $LOGFILE
 
 while read line
 do
