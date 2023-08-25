d6513cc3f5c22c4bb74931fdeb1050dc0b6bf4d1
srinivas <skasetti@gmail.com>
2023-08-25 01:50:00 -0400

    Adding functions

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -2,15 +2,15 @@
 
 APP_LOGS_DIR=/home/centos/app-logs
 
-DATE=$(date +%F)
+DATE=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
-echo "$FILES_TO_DELETE"
+echo "script started executing at $DATE"
 
 while read line
 do
