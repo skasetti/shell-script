41c4b13bc076bea8cfbc1f5eb136283129f49440
srinivas <skasetti@gmail.com>
2023-08-27 18:00:42 -0400

    Added script

--- mail.sh
+++ mail.sh
@@ -14,4 +14,4 @@ ALERT_TYPE=$5
 
 FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY" template.html)
 
-echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
+echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
